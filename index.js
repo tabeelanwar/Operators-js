03f46a453c836f4e8784d28dde4afe8f12670ac5
tabeelanwar <tabeelanwar21@gmail.com>
2025-08-01 14:24:32 -0700

    Operators

---
+++ index.js
@@ -0,0 +1,130 @@
+// var x = 'tabeel' 
+// console.log (x)
+// var x = 'tabeel2'
+// console.log (x)
+// var x = 'tabeel3'
+// console.log (x) //reassure or reasign ho skta hai
+
+// let x = 'tabeel'
+// console.log (x)
+// x = 'tabeel2'
+// console.log (x)
+// x = 'tabeel3'
+// console.log (x) //redeclare nai ho skta lekin reasign multiple times ho skta hai
+
+// const x = 'tabeel'
+// console.log (x)
+// const y = 'tabeel2'
+// console.log (y)
+// const z = 'tabeel3'
+// console.log (z) //no redeclare ho skta hai na reasign ho skta hai 
+
+// var name = 'tabeel'
+// var age = 22 
+// console.log (`my name is ${name} and my age is ${age}`)
+
+// The typeof operator returns the type of a variable or an expression
+
+
+// Data Type 
+
+//  var name = "tabeel"
+//  console.log(typeof name);
+//  var NaN = 
+//  console.log(typeof NaN);
+//  var c = null
+//  console.log(typeof c);
+//  var e = false
+//  console.log(typeof e);
+ 
+// let a = 1934;
+// let b = a.toFixed(4)
+// console.log(b);
+
+// strings
+// Symbol
+// Number
+// undefined
+// Object
+// Boolean
+
+// var name = 'tabeel'
+// console.log(typeof name);
+// var age = 22
+// console.log(typeof age);
+// var NaN = 
+// console.log(typeof NaN);
+// var e = true
+// console.log(typeof e);
+// var a = null
+// console.log(typeof a);
+
+// let a = 2345
+// let b = a.toFixed(2)
+// console.log(b);
+
+
+// JavaScript Arithmetic Operators
+
+// Addition +
+
+// var a = 5
+// var b = 4
+// var x = a + b
+// console.log(x); //result=9
+
+// Subraction -
+
+// var a = 5
+// var b = 4
+// var x = a - b 
+// console.log(x); result=1
+
+// Multiplication *
+
+// var a = 5
+// var b = 4 
+// var x = a * b
+// console.log(x); result = 20
+
+// Exponentiation **
+
+// var a = 4
+// var b = 3
+// var x = a ** b 
+// console.log(x); result = 64
+
+// Division /
+
+// var a = 4
+// var b = 2
+// var x = a / b 
+// console.log(x); result = 2
+
+// Modulus %
+//  var a = 4
+//  var b = 2
+//  var x = a % b 
+//  console.log(x); result = 
+
+// Increment ++
+// var a = 5
+// var b = 4
+// var x = a + + b
+// console.log(x); result = 9
+
+// decrement --
+// var a = 5
+// var b = 4
+// var x = a - - b
+// console.log(x); result = 9
+
+ 
+
+
+// var a = 4
+// var b = 6
+// var c = 100
+// var x = b % c
+// console.log(x);
+
---
+++ practice.js
@@ -0,0 +1,29 @@
+// Arithmetic Operators
+
+var a = 3
+var b = 5
+var c = 2
+var x = (a + b) / c
+console.log(x);
+
+var f = 2
+var d = 5
+var g = 10
+var q = (g / f) - d
+console.log(q);
+
+var h = 4
+var u = 2
+var p = 8
+var m = (p - - h) / u
+console.log(m);
+
+var l = 4
+var r = 5
+var t = (l + r) * (r - - l)
+console.log(t);
+
+var k = 2
+var j = 6
+var v = (j / k) * k - - (j - k)
+console.log(v);
